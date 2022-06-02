a380c38148a9016881f7f184fff462739411a3a3
Alex <alexb@catalystux.com>
2022-06-02 11:48:51 -0700

    Confirm user group delete

--- src/app/views/admin/remove-group-confirm/remove-group-confirm.component.ts
+++ src/app/views/admin/remove-group-confirm/remove-group-confirm.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { UiStateService } from 'src/app/services/ui-state.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'ss-remove-group-confirm',
   templateUrl: './remove-group-confirm.component.html',
-  styleUrls: ['./remove-group-confirm.component.less']
+  styleUrls: ['./remove-group-confirm.component.less'],
 })
 export class RemoveGroupConfirmComponent implements OnInit {
+  @Output() confirmUserGroupDelete = new EventEmitter<string>();
+  constructor(private uiState: UiStateService, private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public close(): void {
+    this.uiState.hideConfirmUserGroupDelete();
   }
 
+  confirm() {
+    this.confirmUserGroupDelete.emit();
+    this.uiState.hideConfirmUserGroupDelete();
+  }
 }
